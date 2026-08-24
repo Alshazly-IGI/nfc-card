3231483ed7c93806779173b2ff95f31e6ef0d7a0
Together we can <asadego800@gmail.com>
2026-08-24 11:52:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("Welcome");
+const qrModal = document.getElementById("qrModal");
+
+qrModal.addEventListener("click", function(event) {
+
+    if (event.target === qrModal) {
+
+        closeQR();
+
+    }
+
+});
